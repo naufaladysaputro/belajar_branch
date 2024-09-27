28fce40ca5a033c0af0741fa7a098fb87a5313ac
naufaladysaputro <157557765+naufaladysaputro@users.noreply.github.com>
2024-09-28 00:54:39 +0700

    tambah fitur kurang

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ function tambah(angka1, angka2) {
     return angka1 + angka2;
   }
   
+  function kurang(angka1, angka2) {
+    return angka1 - angka2;
+  }
+  
   // Contoh penggunaan:
   var hasil = tambah(5, 3);
+  console.log("Hasil penambahan:", hasil); // Output: Hasil penambahan: 8
+
+  var hasil = kurang(5, 3);
   console.log("Hasil penambahan:", hasil); // Output: Hasil penambahan: 8
